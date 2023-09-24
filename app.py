cdc38cc586e65d03a8879dfe7f3d742233f1866e
camenduru <54370274+camenduru@users.noreply.github.com>
2023-09-24 23:19:46 +0300

    Highping

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ with gr.Blocks() as app:
         A space by AP [Follow me on Twitter](https://twitter.com/angrypenguinPNG) with big contributions from [multimodalart](https://twitter.com/multimodalart)
 
         This project works by using [Monster Labs QR Control Net](https://huggingface.co/monster-labs/control_v1p_sd15_qrcode_monster).
-        Given a prompt and your pattern, we use a QR code conditioned controlnet to create a stunning illusion! Credit to: [MrUgleh](https://twitter.com/MrUgleh) for discovering the workflow :)
+        Given a prompt and your pattern, we use a QR code conditioned controlnet to create a stunning illusion! Credit to: [Highping](https://civitai.com/user/Highping) and [MrUgleh](https://twitter.com/MrUgleh) for discovering the workflow :)
         '''
     )
     
